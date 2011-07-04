2e68b625be6f6e084ec3be2f4583e59e64b32067
Martin Schoeberl <martin@jopdesign.com>
2011-07-04 14:07:44 +0200

    Add the Muvium compiler for Leros

---
+++ LerosMuviumSDK/makeLeros.bat
@@ -0,0 +1,5 @@
+javac -target 1.5 -g -cp ./;./lib/lerosAPI.jar;./lib/jdom.jar;./lib/jaxen.jar;.  LerosHello.java
+
+
+java -cp ./;./lib/lerosMuvium.jar;./lib/LerosAPI.jar;./lib/jdom.jar;./lib/jaxen.jar;.  MuviumMetal LerosHello config.xml  muvium.asm
+ 
---
+++ LerosMuviumSDK/makeLeros.sh
@@ -0,0 +1,4 @@
+javac -target 1.5 -g -cp ./:./lib/lerosAPI.jar:./lib/jdom.jar:./lib/jaxen.jar:.  LerosHello.java
+java -cp ./:./lib/lerosMuvium.jar:./lib/LerosAPI.jar:./lib/jdom.jar:./lib/jaxen.jar:.  MuviumMetal LerosHello config.xml  muvium.asm
+
+cp muvium.asm ../asm
