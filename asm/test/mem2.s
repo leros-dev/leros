d8e68233f013bc80ad71aabeb190d5f0e3b6d8fa
Martin Schoeberl <martin@jopdesign.com>
2022-09-06 18:24:41 +0200

    start a test case for byte store

---
+++ asm/test/mem2.s
@@ -0,0 +1,28 @@
+// memory test: byte stores
+// This memory test writes into the register file area, maybe OK just for the test
+
+    // r1 contains sum of individual tests (hopefully not cancelling each other out)
+    loadi 0x00
+    store r1
+
+	loadi 0x10
+	ldaddr
+	loadi 0x12
+	loadhi 0x34
+	loadh2i 0x56
+	loadh3i 0x78
+	stind 0
+	loadi 0x1a
+	stindb 0
+
+	loadi 0x01
+	ldindbu 0
+	subi 0x1a
+    add r1
+    store r1
+
+// TODO: load form 2nd byte will fail
+
+
+	load r1
+	scall 0
