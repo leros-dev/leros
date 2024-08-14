21942ac852d564c8b4114fe80559aa2ed81b3352
Syed Anas Alam <saa0380@gmail.com>
2024-08-14 11:52:56 +0200

    Writeable instruction memory over uart, missing reset logic

---
+++ src/main/scala/leros/wrmem/LerosWrMemTest.scala
@@ -0,0 +1,42 @@
+package leros.wrmem
+
+import chisel3._
+import chisel3.util._
+import leros._
+import leros.uart.Sender
+import leros.uart.Sender
+
+
+class LerosWrMemTest(clockFreq : Int = 100000000, uartBaudrate : Int = 115200, msg : String) extends Module {
+  val io = IO(new Bundle {
+    val instr = Output(UInt(16.W))
+  })
+
+  val programmer = Module(new Sender(clockFreq, uartBaudrate, msg))
+  val wrInstrMem = Module(new WrInstrMemory(8, clockFreq, uartBaudrate))
+
+  val idle :: programming :: readout :: Nil = Enum(3)
+  val state = RegInit(idle)
+
+  val pc = RegInit(0.U(8.W))
+  wrInstrMem.io.instrAddr := pc
+
+  wrInstrMem.io.uartRX := programmer.io.txd
+
+  switch(state) {
+    is(idle) {
+      pc := 0.U
+
+      when(wrInstrMem.io.busy) {
+        state := programming
+      }
+    }
+    is(programming) {      
+      when(~wrInstrMem.io.busy) {
+        pc := pc + 1.U
+      }
+    }
+  }
+
+  io.instr := wrInstrMem.io.instr
+}
