8e98b16f77900a3858c341e36722e6b2ee35d0c7
Martin Schoeberl <martin@jopdesign.com>
2024-09-11 13:22:17 -0700

    Anas example

---
+++ asm/test/anas.s
@@ -0,0 +1,12 @@
+// this is Anas
+
+start:
+    loadi 1     // 0x2101
+    brnz A      // 0xA003
+    loadi 0xFF  // 0x21FF
+    br start    // 0x8FFD
+
+
+A:
+    loadi 0     // 0x2100
+    scall 0     // 0xFF00
